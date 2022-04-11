7a7d8696f949677bac6d472b7db4ec63cbf11b22
fredrikac <fredrika.carlsen@gmail.com>
2022-04-11 09:32:11 +0200

    login & register functions + organized sass

--- frontend/main.js
+++ frontend/main.js
@@ -5,21 +5,83 @@ const audioList = document.querySelector('#audioList');
 
 //fetch all existing books 
 //testar - dessa bör gå att lägga i en och samma funktion
-let getBooks = async()=>{
-  let {data} = await axios.get("http://localhost:1337/api/books?populate=*");
-  console.log(data);
-  renderBookLists(data);
-}
-getBooks();
+// let getBooks = async()=>{
+//   let {data} = await axios.get("http://localhost:1337/api/books?populate=*");
+//   console.log(data);
+//   renderBookLists(data);
+// }
+// getBooks();
 
-let getAudiobooks = async()=>{
-  let {data} = await axios.get("http://localhost:1337/api/audiobooks?populate=*");
-}
-//getAudiobooks();
+// let getAudiobooks = async()=>{
+//   let {data} = await axios.get("http://localhost:1337/api/audiobooks?populate=*");
+// }
+// //getAudiobooks();
 
-let renderBookLists = async (books) => {
-  books.data.forEach(book => {
-    console.log(book)
-    bookList.innerHTML += `Title: ${book.attributes.title} Author: ${book.attributes.author}`
+// let renderBookLists = async (books) => {
+//   books.data.forEach(book => {
+//     console.log(book)
+//     bookList.innerHTML += `Title: ${book.attributes.title} Author: ${book.attributes.author}`
+//   })
+// }
+
+
+
+//LOGIN 
+const loginBtn = document.querySelector('#loginBtn');
+const createBtn = document.querySelector('#createBtn');
+
+//När användaren loggat in vill jag att den ska dirigeras till startsidan
+//Sign in ska ändras till sign out?
+let login = async () => {
+  let username = document.querySelector("#username").value;
+  let password = document.querySelector("#password").value;
+  
+  let response = await axios.post("http://localhost:1337/api/auth/local", {
+    identifier: username,
+    password
   })
+  .then(response => {
+    let token = response.data.jwt;
+    sessionStorage.setItem("token", token);
+    console.log('User logged in! got token: ', token)
+  })
+  .catch(error => {
+    console.log('An error occurred:', error.response);
+  });
 }
+
+loginBtn.addEventListener('click', (e)=> {
+  e.preventDefault();
+  login();
+});
+
+
+//REGISTER NEW USER
+
+//När ny user är skapad, så vill jag att den ska vara inloggad
+let register = async ()=>{
+  let newUser = document.querySelector("#newUser").value;
+  let newPassword = document.querySelector("#userPass").value;
+  let userEmail = document.querySelector('#userEmail').value;
+
+  let response = await axios.post("http://localhost:1337/api/auth/local/register", {
+      username: newUser,
+      password : newPassword,
+      email : userEmail
+  })
+  .then(response =>{
+    let token = response.data.jwt;
+    sessionStorage.setItem("token", token);
+  
+    console.log(`User registration successful!`)
+
+  })
+  .catch(error => {
+    console.log('Oh no! An error occurred:', error.response);
+  });
+}
+
+createBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  register();
+});
